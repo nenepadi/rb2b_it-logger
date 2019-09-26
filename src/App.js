2e3de41931762bf5b33e9a083d25f57fb8562b4a
Nene Padi Ademang <obed.ademang@gmail.com>
2019-09-26 09:13:14 +0000

    Modals on way

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import SearchBar from './components/layout/SearchBar';
+import AddBtn from './components/layout/AddBtn';
+import Logs from './components/logs/Logs';
+import AddLogModal from './components/logs/AddLogModal';
+
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
@@ -7,7 +12,16 @@ const App = () => {
     useEffect(() => {
         M.AutoInit();
     });
-    return <div className='App'>My app</div>;
+    return (
+        <Fragment>
+            <SearchBar />
+            <div className='container'>
+                <AddBtn />
+                <AddLogModal />
+                <Logs />
+            </div>
+        </Fragment>
+    );
 };
 
 export default App;
